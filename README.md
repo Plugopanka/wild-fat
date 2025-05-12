224a520a646ca2d72df3039ad6832f09ecb65026
ionova <ionova@ecopsy.com>
2025-05-12 11:21:33 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# wild-fat
+# Business card website
+
+### О проекте
+Сайт-визитка для гейм-дизайнера
+
+### Опубликованное приложение
+**[Ссылка на сайт](https://тутбудетссылка/)**
+
+---
+
+### Запуск приложения в dev mode
+
+```javascript
+клонируйте репозиторий: "https://github.com/Plugopanka/wild-fat.git"
+
+переходите в папку проекта: cd wild-fat
+
+установите зависимости: npm ci
+
+запустите проект: npm run dev
+
+
+
+```
+
+### Запуск приложения в production mode
+```javascript
+npm run build
+```
+
+### Стек технологий
+
+* HTML 5
+* SCSS modules
+* TypeScript
+* React 18
+
+### Ключевые точки для media queries max-width (1920px)
+
+### Скриншоты
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+//import { Main, About, News, New, Events, Regions, Profile, EventsIdPage } from './components/pages';
+
+function App() {
+    return (
+        <p>hello world</p>
+            // <Routes>
+            //     <Route path="*" element={<Navigate to="/main" />} />
+            //     <Route path="/main" element={<Main />} />
+            //     <Route path="/about" element={<About />} />
+            //     <Route path="/about/:id" element={<AboutIdPage />} />
+            //     <Route path="/news" element={<News />} />
+            //     <Route path="/news/:id" element={<New />} />
+            //     <Route path="/events" element={<Events />} />
+            //     <Route path="/events/:id" element={<EventsIdPage />} />
+            //     <Route path="/regional" element={<Regions />} />
+            //     <Route path="/regional/:id" element={<RegionIdPage />} />
+            //     <Route path="/profile" element={<Profile />} />
+            // </Routes>
+    );
+}
+
+export default App;
