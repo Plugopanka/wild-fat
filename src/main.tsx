da5ab7698d02c84dc1b6ddd96bf0c90940cf3ad8
ionova <ionova@ecopsy.com>
2025-10-04 23:21:54 +0300

    Font import

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import './index.module.scss';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import {BrowserRouter} from 'react-router-dom';
+import './App.module.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        svgr({
-            svgrOptions: {
-                // svgr options
-            },
-        }),
-    ],
-});
+  plugins: [react()],
+  assetsInclude: ['**/*.ttf', '**/*.woff', '**/*.woff2'],
+  publicDir: 'public',
+})
