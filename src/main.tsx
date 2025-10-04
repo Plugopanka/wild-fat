91aa66e2e64fa3ebe1f3a22a7af22ed11ac02a57
ionova <ionova@ecopsy.com>
2025-10-04 23:37:19 +0300

    app css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import './index.module.scss';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import {BrowserRouter} from 'react-router-dom';
-import './App.module.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
