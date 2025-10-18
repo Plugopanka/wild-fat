24d92fc2eff9fdefdddfc9a673c3333ceb8b2f27
ionova <ionova@ecopsy.com>
2025-10-18 23:27:59 +0300

    Add SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import {BrowserRouter} from 'react-router-dom';
 import { LocalizationProvider } from './contexts/LocalizationContext';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <LocalizationProvider>
         <App />
+        <SpeedInsights />
       </LocalizationProvider>
     </BrowserRouter>
   </React.StrictMode>
